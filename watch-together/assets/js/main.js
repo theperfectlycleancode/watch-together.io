6dc929efb11e19407993d82acb1b03bac6a8c9af
theperfectlycleancode <samgamesx.sam@gmail.com>
2019-03-07 06:10:54 +0100

    Video hooking function, early dev stage.

--- watch-together/assets/js/main.js
+++ watch-together/assets/js/main.js
@@ -29,4 +29,25 @@ $(document).ready(function() {
     $('.searching').css("display","block");
     $('.searchmsg').text('Creating room...');
   });
+  const webview = document.querySelector('webview')
+  webview.addEventListener('console-message', (e) => {
+    if(e.message.includes('hooked-link')) {
+      var str = e.message.split('|')[1];
+      console.log(str);
+      webview.loadURL(str);
+    }
+})
+  webview.addEventListener('dom-ready', () => {
+    console.log('test m8');
+
+
+webview.openDevTools();
+
+/*webview.executeJavaScript('  var stream = document.getElementsByTagName("video"); console.log("hooked-link|"+stream[0].baseURI); alert(stream[0].baseURI);',false, function() {
+  console.log('test2');
+});*/
+})
+  $('.testvidin').on('click', function() {
+    webview.executeJavaScript('function getSInfo() { var stream3 = document.getElementsByTagName("video"); for(var i = 0; i < stream3.length; i++) { if(typeof stream3[i].src !== "undefined") { console.log("hooked-link|"+stream3[i].src); } } } getSInfo();')
+  });
 });
