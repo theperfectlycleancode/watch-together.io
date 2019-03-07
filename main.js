b07e2cf4c40d04528625214894f2253d9e663925
theperfectlycleancode <samgamesx.sam@gmail.com>
2019-03-07 03:13:37 +0100

    Hotfix.

--- main.js
+++ main.js
@@ -1,43 +1,67 @@
-<<<<<<< current
-const remote = require('electron').remote;
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
 
-$(document).ready(function() {
-  $('.AppClose').on('click', function() {
-    var window = remote.getCurrentWindow();
-    window.close();;
-  });
-  $('.AppMax').on('click', function() {
-    var window = remote.getCurrentWindow();
-    if (!window.isMaximized()) {
-      window.maximize();
-    } else {
-      window.unmaximize();
-    }
-  });
-  $('.AppMin').on('click', function() {
-    var window = remote.getCurrentWindow();
-    window.minimize();
-  });
-});
-=======
-const remote = require('electron').remote;
+const path = require('path')
+const url = require('url')
 
-$(document).ready(function() {
-  $('.AppClose').on('click', function() {
-    var window = remote.getCurrentWindow();
-    window.close();;
-  });
-  $('.AppMax').on('click', function() {
-    var window = remote.getCurrentWindow();
-    if (!window.isMaximized()) {
-      window.maximize();
-    } else {
-      window.unmaximize();
-    }
-  });
-  $('.AppMin').on('click', function() {
-    var window = remote.getCurrentWindow();
-    window.minimize();
-  });
-});
->>>>>>> before discard
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    minHeight: 420,
+    minWidth: 660,
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    frame: false,
+    backgroundColor: '#36393e'
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'watch-together/views/main.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
